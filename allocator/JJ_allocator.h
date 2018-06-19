d652f3ef3ac5e2224a2675c56c3268f3219005d8
wjhss <w1134132857@gmail.com>
2018-06-19 15:47:06 +0800

    修改：     JJ_allocator.h

--- allocator/JJ_allocator.h
+++ allocator/JJ_allocator.h
@@ -12,15 +12,43 @@ namespace JJ
 template <class T>
 inline T* _allocate(ptrdiff_t size,T*)
 {
-		set_new_handler(0);
-		T* tmp = (T*)(::operator mew((size_t)(size *sizeof(T))));
-		if(tmp==0)
-		{
-			cerr << "out of memory"<<endl;
-			exit(1);
-		}
-		return tmp;
+	set_new_handler(0);
+	T* tmp = (T*)(::operator mew((size_t)(size *sizeof(T))));
+	if(tmp==0)
+	{
+		cerr << "out of memory"<<endl;
+		exit(1);
+	}
+	return tmp;
 }
+template <class T>
+inline void _deallocate(T* buffer)
+{
+	::operator delete(buffer);
+}
+template <class T1,class T2>
+inline void _construct(T1* p,const T2& value)
+{
+	new(p) T1(value);
+}
+template <class T>
+class allocator
+{
+	public:
+		typedef T value_type;
+		typedef T* ponter;
+		typedef const T* const_pointer;
+		typedef T& reference;
+		typedef const T& const_reference;
+		typedef size_t size_type;
+		typedef ptrdiff_t difference_type;
+
+		template <class U>
+		struct rebind
+		{
+			typedef allocator<U> other;
+		};
+
 }
 
 #endif
