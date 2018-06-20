de05adeb6b2d0aac8fd88a81c7a041225fc6e96b
wjhss <w1134132857@gmail.com>
2018-06-20 14:11:56 +0800

    重命名：   allocator/JJ_allocator.h -> allocator/JJalloc.h

--- allocator/JJalloc.h
+++ allocator/JJalloc.h
@@ -32,11 +32,16 @@ inline void _construct(T1* p,const T2& value)
 	new(p) T1(value);
 }
 template <class T>
+inline void _destroy(T *ptr)
+{
+	ptr->~T();
+}
+template <class T>
 class allocator
 {
 	public:
 		typedef T value_type;
-		typedef T* ponter;
+		typedef T* pointer;
 		typedef const T* const_pointer;
 		typedef T& reference;
 		typedef const T& const_reference;
@@ -48,7 +53,26 @@ class allocator
 		{
 			typedef allocator<U> other;
 		};
-
+		pointer allocate(size_type n,const void* hint=0)
+		{
+			return _allocate((difference_type)n,(pointer)0);
+		}
+		void deallocate(pointer p,size_type n){_deallocate(p);}
+		void construct(pointer p,const T & value)
+		{
+			_construct(p,value);
+		}
+		void destroy(pointer p){ _destroy(p);}
+		pointer address(reference x){ return (pointer)&x;}
+		const_pointer const_address(const_reference x)
+		{
+			return (const_pointer)&x;
+		}
+		size_type max_size() const
+		{
+			return size_type(UINT_MAX/sizeof(T);
+		}
+		};
 }
 
 #endif
